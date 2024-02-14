080391f859a9dd28d012781916c5311adeebdf8d
Amlac Eniotna <antoine.calma30@gmail.com>
2024-02-14 16:48:13 +0100

    ajout du footer

---
+++ projet-8/src/react/components/Footer.js
@@ -0,0 +1,12 @@
+import logo from '../../assets/logobw.svg'
+
+function Footer() {
+	return (
+		<footer className="footer">
+			<img src={logo} alt='logo kasa'/>
+			<p>© 2020 Kasa. All rights reserved</p>
+		</footer>
+	)
+}
+
+export default Footer
