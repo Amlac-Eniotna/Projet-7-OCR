ee853b349708fa1d7b9731a5dd9ffb0e4e0e9740
Amlac Eniotna <antoine.calma30@gmail.com>
2024-02-15 16:50:32 +0100

    user-select: none

--- projet-8/src/react/pages/home/Home.jsx
+++ projet-8/src/react/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import Banner from '../../components/Banner'
 import imgBanner from '../../../assets/bannier_index.png'
-import Location from './components/Location';
+import Location from './components/Locations';
 
 function Home(props) {
   return (
