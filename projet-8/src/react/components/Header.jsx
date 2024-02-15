f3b4a343b2cca00380398f921a7be6126dfb736b
Amlac Eniotna <antoine.calma30@gmail.com>
2024-02-15 19:23:50 +0100

    ajout useState => selection navbar + animation

--- projet-8/src/react/components/Header.jsx
+++ projet-8/src/react/components/Header.jsx
@@ -1,13 +1,22 @@
 import logo from '../../assets/logo.svg'
+import { useState } from 'react'
 
 function Header() {
+    const [menuAcceuil, updateAcceuil] = useState(true)
+    const [menuAPropos, updateAPropos] = useState(false)
     return (
         <header className='header'>
             <img src={logo} alt='logo kasa' className='header__img'/>
             <nav className='header__nav'>
                 <ul className='header__nav--ul'>
-                    <li>Accueil</li>
-                    <li>À Propos</li>
+                    {menuAcceuil ? <li className='menu-select'>Accueil</li> : <li onClick={() => {
+                        updateAcceuil(true)
+                        updateAPropos(false)
+                        }}>Accueil</li>}
+                    {menuAPropos ? <li className='menu-select'>À Propos</li> : <li onClick={() => {
+                        updateAcceuil(false)
+                        updateAPropos(true)
+                        }}>À Propos</li>}
                 </ul>
             </nav>
         </header>
