fe417a6a5e0fd9673f84cf96ca29cafc3259e43b
Amlac Eniotna <antoine.calma30@gmail.com>
2024-02-20 18:48:20 +0100

    ajout de <Navigate/> pour rediriger / vers /home

--- projet-8/src/react/pages/home/Home.jsx
+++ projet-8/src/react/pages/home/Home.jsx
@@ -2,10 +2,13 @@ import Banner from '../../components/Banner'
 import imgBanner from '../../../assets/bannier_index.png'
 import Location from './components/Locations'
 import Header from '../../components/Header'
+import { Navigate, useLocation } from 'react-router-dom'
 
 function Home(props) {
+  const location = useLocation()
   return (
     <>
+      {location.pathname === '/' ? <Navigate to="/home" /> : null}
       <Header linkAPropos={false} linkAccueil={true} />
       <Banner titre="Chez vous, partout et ailleurs" img={imgBanner} />
       <Location logements={props.logements} />
