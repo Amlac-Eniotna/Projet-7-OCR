ae156c26cacce41f06a04c3d703c94070e06f723
Amlac Eniotna <antoine.calma30@gmail.com>
2024-02-20 19:55:37 +0100

    404 <navigate/> /* vers /404

--- src/react/pages/404/404.jsx
+++ src/react/pages/404/404.jsx
@@ -1,10 +1,13 @@
 import { Link } from 'react-router-dom'
 import Header from '../../components/Header'
+import { Navigate, useLocation } from 'react-router-dom'
 
 function NotFound() {
+  const location = useLocation()
   return (
     <>
       <Header />
+      {location.pathname !== '/404' ? <Navigate to="/404" /> : null}
       <section className="erreur-404">
         <h1 className="erreur-404__h1">404</h1>
         <h2 className="erreur-404__h2">
